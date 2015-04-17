835c736597428352541f108c50623664a0148ace
junhui.xu <junhui.xu@travelzen.com>
2015-04-17 19:44:44 +0800

    first commit

---
+++ dkzj-front/src/main/java/com/dkzj/front/controller/Welcome.java
@@ -0,0 +1,14 @@
+package com.dkzj.front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Welcome {
+	@RequestMapping("/index")
+	public String index(Model model){
+		model.addAttribute("username", "junhui.xu");
+		return "page/index";
+	}
+}
