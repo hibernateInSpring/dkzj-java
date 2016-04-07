8772a1943e77170ded6eaaf3f72133219c2faf66
junhui.xu <junhui.xu@travelzen.com>
2016-04-07 15:55:52 +0800

    mvc

--- dkzj-front/src/main/java/com/dkzj/front/controller/Welcome.java
+++ dkzj-front/src/main/java/com/dkzj/front/controller/Welcome.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
+@RequestMapping("/main")
 public class Welcome {
+
 	@RequestMapping("/index")
 	public String index(Model model){
 		model.addAttribute("username", "junhui.xu");
